6a92a753b67072c076a3ed8cde4285235c46a467
NavyaSai-V <navyasai1401@gmail.com>
2024-10-02 08:07:36 +0530

    echo bot creation

--- Chatbot_main.py
+++ Chatbot_main.py
@@ -8,14 +8,25 @@ import google.generativeai as gen_ai
 
 load_dotenv()
 
-with st.chat_message("user"):
-    st.write("Hello ...")
+st.title("Echo Bot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# prompt = st.chat_input("say something")
+
+if prompt:= st.chat_input("say something"):
+
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+response = f"Echo: {prompt}"
 
 with st.chat_message("assistant"):
-    st.write("I am assistant")
-    st.bar_chart(np.random.rand(30,3))
+    st.markdown(response)
 
-prompt = st.chat_input("say something")
-
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+st.session_state.messages.append({"role": "assistant", "content": response})
